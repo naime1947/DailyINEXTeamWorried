f4eaf6a5461adddee153c863b1ed6f3a0a646844
Naime Ahmed <naime1947@gmail.com>
2019-05-23 01:46:13 +0600

    Company Class model created, Country Class Created. Country Class Dtabase also loaded

---
+++ DailyINEX/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DailyINEX.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registraion  
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //Login
+    }
+}
---
+++ DailyINEX/Models/Compnay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DailyINEX.Models
+{
+    public class Compnay
+    {
+        [Display(Name ="Company Name")]
+        [Required]
+        public string CompnayName { get; set; }
+
+        [Display(Name = "Company Email")]
+        [Required]
+        public string CompnayEmail { get; set; }
+
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage ="Minimum password lenth is 6 charecter")]
+        public string Password { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password can confirm password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name ="Country")]
+        [Required]
+        public int CountryId { get; set; }
+    }
+
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
